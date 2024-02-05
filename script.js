7160fb287cc90b12a79057e651146ed40acf928d
Juan Diego Teheran Olmos <jdteheran@uninorte.edu.co>
2024-02-05 18:51:34 -0500

    cambio programador B

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ let primoTemp = 1
 
 while (i < cantidad) {
     
-    if (esPrimo(primoTemp)) {
+    if (esPrimo(primoTemp) && i > -1) {
         console.log(i+1,"->",primoTemp);
         i++
     }
